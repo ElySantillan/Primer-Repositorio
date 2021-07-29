f99382391ce7b00c8608e6012d0e19b05584f04b
Ely Santillan Mtz <63132348+ElySantillan@users.noreply.github.com>
2021-07-29 18:59:46 -0500

    Codigo js de par y impar

---
+++ app.js
@@ -0,0 +1,25 @@
+/*Funcion*/ /* parametros de entrada*/
+/* alert("Hola Mundo"); */
+
+
+let numero = parseInt(prompt("Porfavor ingresa un numero entero:"));
+
+if (numero % 2 == 0) {
+    alert("El numero es PAR")
+} else {
+    alert("El numero es IMPAR")
+}
+
+/* % == modulo residuo  */
+/* == es igual igual*/
+/*parseInt (cambiar cadenas a numero solo se pone esa palabra)*/ 
+
+/*tipo de datos en javaScript*/
+/* modulo*/
+/* \n para poder salto de line*/
+/* \t para espacion grande de tabulador*/
+/* ``  para hacer templeta String */
+/*
+let permite crear  una variable
+variable un dato que me puede cambiar a traves del tiempo en ejecucion del programa
+*/
